d7cf11bb187a456499e661862343b0e032d406b8
xiaobo <xiaoboliyaqing@163.com>
2018-09-19 17:48:04 +0800

    大量引入bootstrap样式进行了美观，修复了头像展示Bug

--- src/main/java/com/xiaobo/controller/userController.java
+++ src/main/java/com/xiaobo/controller/userController.java
@@ -3,70 +3,86 @@ package com.xiaobo.controller;
 import com.xiaobo.bean.User;
 import com.xiaobo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class userController {
     @Autowired
     IUserService iu;
+    //资源加载器
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     //注册并且上传头像文件
     @PostMapping("/registe")
     public String registe(@Valid User user, BindingResult result, MultipartFile uploadFile, Model model) throws IOException {
         user.setImgPath(uploadFile.getOriginalFilename());
         iu.save(user);
-        model.addAttribute("msg","恭喜您注册成功,请登录吧~");
-        uploadFile.transferTo(new File("D://uploadDir/"+uploadFile.getOriginalFilename()));
+        model.addAttribute("msg", "恭喜您注册成功,请登录吧~");
+        uploadFile.transferTo(new File("D://uploadDir/" + uploadFile.getOriginalFilename()));
         return "login";
     }
 
     //检验用户名是否存在
     @GetMapping("/checkUserName")
     @ResponseBody
-    public String checkUserName(String username){
+    public String checkUserName(String username) {
         List<User> all = iu.findAll();
         for (User user : all) {
             System.out.println(user.getName());
-            if(user.getName().equals(username)){
-               return "该用户名存在";
+            if (user.getName().equals(username)) {
+                return "该用户名存在";
             }
         }
-        if(username != ""){
+        if (username != "") {
             return "该用户名不存在";
-        }else{
+        } else {
             return "用户名不能为空";
         }
     }
 
     //登录
     @PostMapping("/toLogin")
-    public String login(String name,String password,Model model){
+    public String login(String name, String password, Model model) {
         List<User> all = iu.findAll();
         for (User user : all) {
-            if(user.getName().equals(name)&user.getPassword().equals(password)){
-                model.addAttribute("msg",name);
-               return "index";
+            if (user.getName().equals(name) & user.getPassword().equals(password)) {
+                model.addAttribute("msg", name);
+                model.addAttribute("list", all);
+                return "index";
             }
         }
-        model.addAttribute("msg","登录失败，用户名或者密码错误");
+        model.addAttribute("msg", "登录失败，用户名或者密码错误");
         return "login";
     }
 
     @RequestMapping("/loginUI")
-    public String loginUI(){
+    public String loginUI() {
         return "login";
+    }
+
+    //获取图片
+    @GetMapping("getImage/{imgPath:.+}")
+    public ResponseEntity getImg(@PathVariable String imgPath) {
+        System.out.println(imgPath+"------------");
+        Path path = Paths.get("D://uploadDir", imgPath);
+        System.out.println(path.toString()+"------------");
+        Resource resource = resourceLoader.getResource("file:" + path.toString());
+        return ResponseEntity.ok(resource);
     }
 
 }
